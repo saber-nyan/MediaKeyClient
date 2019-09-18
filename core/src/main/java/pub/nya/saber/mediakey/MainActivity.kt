1f36f8cb799a25477542ca8f1244527aed136c9b
saber-nyan <saber-nyan@ya.ru>
2019-09-18 23:26:18 +0300

    Добавил настройки

--- core/src/main/java/pub/nya/saber/mediakey/MainActivity.kt
+++ core/src/main/java/pub/nya/saber/mediakey/MainActivity.kt
@@ -1,12 +1,28 @@
 package pub.nya.saber.mediakey
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = this::class.java.simpleName
+
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        preferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        Button_openSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
+        Log.v(tag, "Prefs contains ${preferences.all}")
     }
 }
